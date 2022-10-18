5fab99892e8e44247fc5567fcded84895334e61a
Fabian Simon <77444050+fabiansimon@users.noreply.github.com>
2022-10-18 22:05:35 +0100

    __

--- src/screens/Intro/SignUpScreen.js
+++ src/screens/Intro/SignUpScreen.js
@@ -1,6 +1,9 @@
-import { View, StyleSheet } from 'react-native';
+import {
+  View, StyleSheet, SafeAreaViewBase, ScrollView,
+} from 'react-native';
 import React, { useState, useRef } from 'react';
 import Animated from 'react-native-reanimated';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import COLORS, { PADDING } from '../../constants/Theme';
 import i18n from '../../utils/i18n';
 import PollView from '../../components/Polls/PollView';
@@ -18,132 +21,137 @@ import KeyboardView from '../../components/KeyboardView';
 import Button from '../../components/Button';
 
 export default function SignUpScreen() {
-  const scrollY = useRef(new Animated.Value(0)).current;
-  const [isVisible, setIsVisible] = useState(0);
+  const errorColors = {
+    error: COLORS.error[900],
+    success: COLORS.success[700],
+    neutral: COLORS.neutral[300],
+  };
+  const [isVisible, setIsVisible] = useState(false);
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
+  const [errorChecks, setErrorChecks] = useState([
+    {
+      firstName: {
+        error: i18n.t('missing'),
+        color: errorColors.neutral,
+      },
+    },
+  ]);
 
-  const InputContainer = () => (
-    <View style={{ marginHorizontal: PADDING.l, marginTop: 42 }}>
-      <View style={{ flexDirection: 'row', flex: 1 }}>
-        <View style={{ flex: 1, paddingRight: 8 }}>
-          <Body
-            color={COLORS.neutral[700]}
-            type={2}
-            style={{ marginBottom: 6, marginLeft: 5 }}
-            text={i18n.t('First name')}
-          />
-          <TextField
-            label={i18n.t('First name')}
-            value={firstName || null}
-            onChangeText={(val) => setFirstName(val)}
-            placeholder={i18n.t('John')}
-          />
-        </View>
-        <View style={{ flex: 1 }}>
-          <Body
-            color={COLORS.neutral[700]}
-            style={{ marginBottom: 6, marginLeft: 5 }}
-            type={2}
-            text={i18n.t('Last name')}
-          />
-          <TextField
-            value={lastName || null}
-            onChangeText={(val) => setLastName(val)}
-            placeholder={i18n.t('Doe')}
-          />
-        </View>
-      </View>
-      <Divider vertical={16} />
-      <View style={{ flex: 1 }}>
-        <Body
-          color={COLORS.neutral[700]}
-          style={{ marginBottom: 6, marginLeft: 5 }}
-          type={2}
-          text={i18n.t('Email')}
-        />
-        <TextField
-          onChangeText={(val) => setEmail(val)}
-          value={email || null}
-          placeholder={i18n.t('john-doe@email.com')}
-        />
-      </View>
+  const CheckList = () => (
+    <View style={{ marginBottom: 10 }}>
+      <Body
+        type={2}
+        text={`• ${i18n.t('First name')}`}
+      />
+      <Body
+        type={2}
+        text={`• ${i18n.t('Last name')}`}
+      />
+      <Body
+        type={2}
+        text={`• ${i18n.t('Email')}`}
+      />
     </View>
   );
 
-  const CheckList = () => {
-    const checkColors = {
-      error: COLORS.error[900],
-      success: COLORS.success[700],
-      neutral: COLORS.neutral[300],
-    };
-
-    return (
-      <View>
-        <Body
-          type={2}
-          text={`• ${i18n.t('First name')} ${[0].}`}
-        />
-        <Body
-          type={2}
-          text="• First name is valid"
-        />
-        <Body
-          type={2}
-          text="• First name is valid"
-        />
-      </View>
-    );
-  };
-
   return (
-    <View style={styles.container}>
-      <HybridHeader
-        title={i18n.t('Sign up')}
-        scrollY={scrollY}
-        info={INFORMATION.dateScreen}
-        backButton={false}
-        backgroundColor={COLORS.neutral[50]}
-        content={(
-          <View style={{ flexDirection: 'row', marginLeft: PADDING.l, marginTop: -12 }}>
+
+    <KeyboardView>
+      <SafeAreaView style={{ backgroundColor: COLORS.neutral[50], flex: 1, justifyContent: 'space-between' }}>
+        <View style={styles.container}>
+          <Headline
+            text={i18n.t('Sign up!')}
+            type={2}
+          />
+          <View style={{ flexDirection: 'row', marginTop: 6 }}>
             <Body text={i18n.t('Already have an account?')} />
             <Body
               style={{ marginHorizontal: 3 }}
               text={i18n.t('Click')}
             />
             <Body
-              onPress={() => setIsVisible(1)}
+              onPress={() => setIsVisible(true)}
               text={i18n.t('here')}
               style={{ textDecorationLine: 'underline', fontWeight: '500' }}
             />
+
           </View>
-)}
-      >
-        <KeyboardView>
-          <InputContainer />
-          <View style={{ marginHorizontal: PADDING.l }}>
-            <CheckList />
+          <View style={{ marginTop: 42 }}>
+            <View style={{ flexDirection: 'row' }}>
+              <View style={{ flex: 1, paddingRight: 8 }}>
+                <Body
+                  color={COLORS.neutral[700]}
+                  type={2}
+                  style={{ marginBottom: 6, marginLeft: 5 }}
+                  text={i18n.t('First name')}
+                />
+                <TextField
+                  label={i18n.t('First name')}
+                  value={firstName || null}
+                  onChangeText={(val) => setFirstName(val)}
+                  placeholder={i18n.t('John')}
+                />
+              </View>
+
+              <View style={{ flex: 1 }}>
+                <Body
+                  color={COLORS.neutral[700]}
+                  type={2}
+                  style={{ marginBottom: 6, marginLeft: 5 }}
+                  text={i18n.t('Last name')}
+                />
+                <TextField
+                  label={i18n.t('Last name')}
+                  value={lastName || null}
+                  onChangeText={(val) => setLastName(val)}
+                  placeholder={i18n.t('Doe')}
+                />
+              </View>
+            </View>
+            <View style={{ marginTop: 12 }}>
+              <Body
+                color={COLORS.neutral[700]}
+                type={2}
+                style={{ marginBottom: 6, marginLeft: 5 }}
+                text={i18n.t('Email')}
+              />
+              <TextField
+                label={i18n.t('Email')}
+                value={email || null}
+                onChangeText={(val) => setEmail(val)}
+                placeholder={i18n.t('Your Email')}
+              />
+            </View>
+          </View>
+        </View>
+        <View style={styles.footer}>
+          <CheckList />
+          <View style={{ width: '100%', height: 60 }}>
             <Button
-              text={i18n.t('Next')}
               fullWidth
-              onPress={() => setIsVisible(2)}
+              onPress={() => setIsVisible(true)}
+              text={i18n.t('Next')}
             />
           </View>
-        </KeyboardView>
-      </HybridHeader>
+        </View>
+      </SafeAreaView>
       <AuthModal
-        isVisible={isVisible === 1 || isVisible === 2}
+        isVisible={isVisible}
         onRequestClose={() => setIsVisible(false)}
         isRegister
       />
-    </View>
+    </KeyboardView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: COLORS.neutral[50],
+    marginTop: 24,
+    marginHorizontal: PADDING.l,
+  },
+  footer: {
+    marginHorizontal: PADDING.l,
   },
 });
