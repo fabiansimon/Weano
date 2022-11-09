462f770b893ca2266cafadec7f9a5b3ff5cb58ff
Fabian Simon <77444050+fabiansimon@users.noreply.github.com>
2022-11-09 16:31:41 +0000

    updated stuff

--- src/components/ContactsModal.js
+++ src/components/ContactsModal.js
@@ -1,14 +1,82 @@
 import {
-  StyleSheet, ScrollView,
+  StyleSheet, ScrollView, TouchableOpacity, View,
 } from 'react-native';
-import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import React, { useState, useEffect } from 'react';
 import TitleModal from './TitleModal';
 import i18n from '../utils/i18n';
-import ContactTile from './ContactTile';
+import COLORS, { PADDING } from '../constants/Theme';
+import Body from './typography/Body';
 
 export default function ContactsModal({
-  isVisible, onRequestClose, data, onAdd,
+  isVisible, onRequestClose, data,
 }) {
+  const [selectedContacts, setSelectedContacts] = useState([]);
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+  const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+
+  useEffect(() => {
+    setSelectedContacts(data);
+  }, []);
+
+  const getNumberTile = (n) => (
+    <TouchableOpacity
+      style={styles.numberContainer}
+
+    >
+      <View>
+        <Body
+          text={capitalize(n.label)}
+          type={3}
+        />
+        <Body
+          text={n.number}
+          type={3}
+          color={COLORS.neutral[700]}
+        />
+      </View>
+      {/* {selectedNumber === n.number && (
+      <Icon
+        name="check"
+        size={22}
+      />
+      )} */}
+    </TouchableOpacity>
+  );
+
+  const ContactTile = ({ contact, index }) => {
+    const expandable = contact.phoneNumbers.length > 1;
+
+    return (
+      <View>
+        <TouchableOpacity
+          onPress={() => (expandable ? setExpandedIndex((prev) => (prev === index ? -1 : index)) : addInvitee(contact))}
+          activeOpacity={0.7}
+          style={[styles.container, { borderTopWidth: index !== 0 && 1 }]}
+        >
+          <Body
+            type={3}
+            text={`${contact.givenName} ${contact.familyName}`}
+            color={COLORS.neutral[700]}
+          />
+          {expandable
+          && (
+            <Icon
+              name="chevron-down"
+              size={24}
+              color={COLORS.neutral[300]}
+            />
+          )}
+        </TouchableOpacity>
+        {expandedIndex === index && (
+        <View>
+          {contact.phoneNumbers.map((n) => getNumberTile(n, contact))}
+        </View>
+        )}
+      </View>
+    );
+  };
+
   return (
     <TitleModal
       isVisible={isVisible}
@@ -24,9 +92,20 @@ export default function ContactsModal({
         contentContainerStyle={{ paddingBottom: 50 }}
       >
         {data.map((contact, index) => (
-          <ContactTile contact={contact} index={index} onAdd={(c) => onAdd(c)} />
+          <ContactTile contact={contact} index={index} />
         ))}
       </ScrollView>
     </TitleModal>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    borderTopColor: COLORS.neutral[100],
+  },
+});
